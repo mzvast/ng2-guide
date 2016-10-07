e893ad5d4a6b6f0b2b266699d099cd85415287fd
mzvast <mzvast@gmail.com>
2016-10-07 22:47:17 +0800

    fix ts missing err

--- dependency-injection/app/hero.service.ts
+++ dependency-injection/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Optional } from '@angular/core';
 import { HEROES }     from './mock-heroes';
-
+import { Logger } from './logger.service';
 @Injectable()
 export class HeroService {
   constructor(
