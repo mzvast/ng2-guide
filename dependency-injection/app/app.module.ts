7aee2aa1a5fecfa4917d8a84914927c89b9c0546
mzvast <mzvast@gmail.com>
2016-10-07 22:22:00 +0800

    OpaqueToken

--- dependency-injection/app/app.module.ts
+++ dependency-injection/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { UserService } from './user.service';
 import { Logger, EvenBetterLogger } from './logger.service';
+import { APP_CONFIG, HERO_DI_CONFIG } from './app-config';
 
 import { AppComponent }  from './app.component';
 import { HeroesComponent } from './heroes.component';
@@ -13,7 +14,8 @@ import { HeroListComponent } from './hero-list.component';
   providers: [
     UserService,
     EvenBetterLogger,
-    {provide: Logger, useExisting: EvenBetterLogger}],
+    {provide: Logger, useExisting: EvenBetterLogger},
+    { provide: APP_CONFIG, useValue: HERO_DI_CONFIG }],
   declarations: [ AppComponent, HeroesComponent, HeroListComponent],
   bootstrap: [ AppComponent ]
 })
--- dependency-injection/app/heroes.component.ts
+++ dependency-injection/app/heroes.component.ts
@@ -1,11 +1,17 @@
-import { Component }          from '@angular/core';
+import { APP_CONFIG, AppConfig } from './app-config';
+import { Component, Inject }          from '@angular/core';
 import { heroServiceProvider } from './hero.service.provider';
 @Component({
   selector: 'my-heroes',
   providers: [heroServiceProvider],
   template: `
-  <h2>Heroes</h2>
+  <h2>{{title}}</h2>
   <hero-list></hero-list>
   `
 })
-export class HeroesComponent { }
+export class HeroesComponent {
+  title: string;
+  constructor(@Inject(APP_CONFIG) config: AppConfig) {
+    this.title = config.title;
+  }
+ }
